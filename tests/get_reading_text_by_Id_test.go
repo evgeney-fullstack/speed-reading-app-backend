b7b896c3f27d7d946a04c956cdfda4adec7fb484
evgeney-fullstack <evgeney.kovalev@mail.ru>
2025-12-11 14:38:47 +0300

    test: add integration test for retrieves a reading text by its ID endpoint - Implement TestGetReadingTextByIdIntegration - Test both success and error cases - Validate request/response flow

--- tests/get_reading_text_by_Id_test.go
+++ tests/get_reading_text_by_Id_test.go
@@ -4,17 +4,17 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// TestCreateReadingTextIntegration is testing the endpoint of create a new reading text
-func TestCreateReadingTextIntegration(t *testing.T) {
+// TestgetReadingTextByIdIntegration is testing the endpoint of create a new reading text
+func TestGetReadingTextByIdIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping integration tests in short mode")
 	}
@@ -35,6 +35,7 @@ func TestCreateReadingTextIntegration(t *testing.T) {
 	tests := []struct {
 		name           string
 		payload        interface{}
+		parameter      string
 		contextTimeout time.Duration
 		expectedStatus int
 		checkResponse  func(t *testing.T, recorder *httptest.ResponseRecorder)
@@ -66,97 +67,83 @@ func TestCreateReadingTextIntegration(t *testing.T) {
 					},
 				},
 			},
+			parameter:      "1",
 			contextTimeout: 15 * time.Second,
 			expectedStatus: http.StatusCreated,
 			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
-				assert.Contains(t, recorder.Body.String(), "{\"id\":1}")
+				assert.Contains(t, recorder.Body.String(), "\"id\":1")
 			},
 		},
 		{
-			name: "Error -  invalid request body",
-			payload: map[string]interface{}{
-				"content":   "test test test test.",
-				"questions": "question1",
+			name:           "Successful get reading text by id",
+			parameter:      "1",
+			contextTimeout: 15 * time.Second,
+			expectedStatus: http.StatusOK,
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				assert.Contains(t, recorder.Body.String(), "test test test test.")
 			},
+		},
+		{
+			name:           "Error - invalid text_id param",
+			parameter:      ":1",
 			contextTimeout: 15 * time.Second,
 			expectedStatus: http.StatusBadRequest,
 			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
-				assert.Contains(t, recorder.Body.String(), "invalid_request_body", "Invalid request body format")
+				assert.Contains(t, recorder.Body.String(), "Invalid request param format")
 			},
 		},
 		{
-			name: "Error - empty content",
-			payload: map[string]interface{}{
-				"content": "",
-				"questions": []map[string]interface{}{
-					{
-						"question": "question1",
-						"answer":   true,
-					},
-					{
-						"question": "question2",
-						"answer":   false,
-					},
-					{
-						"question": "question3",
-						"answer":   false,
-					},
-					{
-						"question": "question4",
-						"answer":   true,
-					},
-					{
-						"question": "question5",
-						"answer":   false,
-					},
-				},
-			},
+			name:           "Error - invalid text_id value",
+			parameter:      "0",
 			contextTimeout: 15 * time.Second,
 			expectedStatus: http.StatusBadRequest,
 			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
-				assert.Contains(t, recorder.Body.String(), "invalid_request_body", "Invalid request body format")
+				assert.Contains(t, recorder.Body.String(), "Text ID must be positive")
 			},
 		},
 		{
-			name: "Error - request timeout",
-			payload: map[string]interface{}{
-				"content": strings.Repeat("test ", 1000000), // Big Data
-				"questions": []map[string]interface{}{
-					{
-						"question": "question1",
-						"answer":   true,
-					},
-					{
-						"question": "question2",
-						"answer":   false,
-					},
-					{
-						"question": "question3",
-						"answer":   false,
-					},
-					{
-						"question": "question4",
-						"answer":   true,
-					},
-					{
-						"question": "question5",
-						"answer":   false,
-					},
-				},
-			},
-			contextTimeout: 100 * time.Millisecond,
-			expectedStatus: http.StatusRequestTimeout,
+			name:           "Error - invalid text_id value",
+			parameter:      "-1",
+			contextTimeout: 15 * time.Second,
+			expectedStatus: http.StatusBadRequest,
 			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
-				assert.Contains(t, recorder.Body.String(), "request_timeout", "Operation timed out")
+				assert.Contains(t, recorder.Body.String(), "Text ID must be positive")
 			},
 		},
 	}
 
+	t.Run("Successful creation", func(t *testing.T) {
+		// Request preparation
+		body, _ := json.Marshal(tests[0].payload)
+		req, _ := http.NewRequest("POST", "/reading_text/", bytes.NewBuffer(body))
+		req.Header.Set("Content-Type", "application/json")
+
+		// Setting the context with timeout
+		ctx, cancel := context.WithTimeout(context.Background(), tests[0].contextTimeout)
+		defer cancel()
+		req = req.WithContext(ctx)
+
+		// Request execution
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, req)
+
+		// Checking the response status
+		assert.Equal(t, tests[0].expectedStatus, recorder.Code)
+
+		// Checking the response body
+		if tests[0].checkResponse != nil {
+			tests[0].checkResponse(t, recorder)
+		}
+	})
+
 	for _, tt := range tests {
+		if tt.name == "Successful creation" {
+			continue
+		}
 		t.Run(tt.name, func(t *testing.T) {
 			// Request preparation
 			body, _ := json.Marshal(tt.payload)
-			req, _ := http.NewRequest("POST", "/reading_text/", bytes.NewBuffer(body))
+			req, _ := http.NewRequest("GET", fmt.Sprintf("/reading_text/%s", tt.parameter), bytes.NewBuffer(body))
 			req.Header.Set("Content-Type", "application/json")
 
 			// Setting the context with timeout
